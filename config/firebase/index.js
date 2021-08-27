1f89fcca52ce8b945ada1ca4130fc801355c980b
Fabio <fabiopmfilho@outlook.com>
2021-08-27 02:29:20 -0300

    fix: export firebase config

--- config/firebase/index.js
+++
@@ -1,5 +0,0 @@
-// import firebaseServer from './server'
-import firebaseClient, { persistenceMode } from './client'
-import firebaseServer from './server'
-
-export { firebaseClient, firebaseServer, persistenceMode }
